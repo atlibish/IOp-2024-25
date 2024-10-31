56e21947ea411074c9175fb1c5b88214f4da055f
Dorian Konwinski <atli@mat.umk.pl>
2024-10-31 10:47:01 +0100

    added check for non-positive numbers

--- liczba.cpp
+++ liczba.cpp
@@ -2,10 +2,23 @@
 
 using namespace std;
 
+bool czy_bezkwadratowa(int n)
+{
+	for (int i = 2; i * i <= n; i++)
+		if (n % (i * i) == 0)
+			return false;
+	return true;
+}
+
 int main()
 {
 	int n;
 	cin >> n;
-	cout << n << endl;
+	if (n <= 0)
+	{
+		cout << "Liczba jest niedodatnia." << endl;
+		return 1;
+	}
+	cout << n << (czy_bezkwadratowa(n) ? " jest bezkwadratowa." : " nie jest bezkwadratowa.") << endl;
 	return 0;
 }
