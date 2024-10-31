9606ad0fd1c958aa4ca7661291ed3ab23a6f01be
Dorian Konwinski <atli@mat.umk.pl>
2024-10-31 10:41:55 +0100

    added func czy_bezkwadratowa()

--- liczba.cpp
+++ liczba.cpp
@@ -2,10 +2,18 @@
 
 using namespace std;
 
+bool czy_bezkwadratowa(int n)
+{
+	for (int i = 2; i * i <= n; i++)
+		if (n % (i * i) == 0)
+			return false;
+	return true;
+}
+
 int main()
 {
 	int n;
 	cin >> n;
-	cout << n << endl;
+	cout << n << (czy_bezkwadratowa(n) ? " jest bezkwadratowa." : " nie jest bezkwadratowa.") << endl;
 	return 0;
 }
